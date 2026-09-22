be1532dabc2410a0554968efe5f1941a593fc20d
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-09-22 23:23:24 +0530

    Refactored: RishuMusic to codex

--- codex/utils/inline/extras.py
+++ codex/utils/inline/extras.py
@@ -1,26 +1,4 @@
-# Copyright (c) 2025 @SUDEEPBOTS <HellfireDevs>
-# Location: delhi,noida
-#
-# All rights reserved.
-#
-# This code is the intellectual SUDEEPBOTS.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: sudeepgithub@gmail.com
-
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.enums import ButtonStyle
 
 from config import SUPPORT_CHAT
 
@@ -28,8 +6,8 @@ from config import SUPPORT_CHAT
 def botplaylist_markup(_):
     buttons = [
         [
-            InlineKeyboardButton(text=_["S_B_9"], url=SUPPORT_CHAT, style=ButtonStyle.PRIMARY, icon_custom_emoji_id="5397733426654626788"),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close", style=ButtonStyle.PRIMARY, icon_custom_emoji_id="5397733426654626788"),
+            InlineKeyboardButton(text=_["S_B_9"], url=SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
     ]
     return buttons
@@ -42,7 +20,6 @@ def close_markup(_):
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
                     callback_data="close",
-                    style=ButtonStyle.PRIMARY, icon_custom_emoji_id="5397733426654626788"
                 ),
             ]
         ]
@@ -57,7 +34,6 @@ def supp_markup(_):
                 InlineKeyboardButton(
                     text=_["S_B_9"],
                     url=SUPPORT_CHAT,
-                    style=ButtonStyle.PRIMARY, icon_custom_emoji_id="5397733426654626788"
                 ),
             ]
         ]
