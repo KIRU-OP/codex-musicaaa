0838542dfff5951c3b0ff9fd8b3b8d5cca2045c1
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-09-22 23:24:10 +0530

    Refactored: RishuMusic to codex

--- codex/plugins/admins/skip.py
+++ codex/plugins/admins/skip.py
@@ -1,37 +1,15 @@
-# Copyright (c) 2025 @SUDEEPBOTS <HellfireDevs>
-# Location: delhi,noida
-#
-# All rights reserved.
-#
-# This code is the intellectual SUDEEPBOTS.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: sudeepgithub@gmail.com
-
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 
 import config
 from codex import YouTube, app
-from codex.core.call import Sagar
+from codex.core.call import Anony
 from codex.misc import db
 from codex.utils.database import get_loop
 from codex.utils.decorators import AdminRightsCheck
 from codex.utils.inline import close_markup, stream_markup
-from codex.utils.stream.autoclear import auto_clean, auto_turn
 from codex.utils.thumbnails import get_thumb
-from config import BANNED_USERS
+from config import BANNED_USERS,autoclean
 
 
 @app.on_message(
@@ -59,23 +37,21 @@ async def skip(cli, message: Message, _, chat_id):
                             except:
                                 return await message.reply_text(_["admin_12"])
                             if popped:
-                                await auto_clean(popped)
+                                rem = popped["file"]
+                                autoclean.remove(rem)
                             if not check:
-                                await auto_turn(chat_id, popped)
-                                check = db.get(chat_id)
-                                if not check:
-                                    try:
-                                        await message.reply_text(
-                                            text=_["admin_6"].format(
-                                                message.from_user.mention,
-                                                message.chat.title,
-                                            ),
-                                            reply_markup=close_markup(_),
-                                        )
-                                        await Sagar.stop_stream(chat_id)
-                                    except:
-                                        return
-                                    break
+                                try:
+                                    await message.reply_text(
+                                        text=_["admin_6"].format(
+                                            message.from_user.mention,
+                                            message.chat.title,
+                                        ),
+                                        reply_markup=close_markup(_),
+                                    )
+                                    await Anony.stop_stream(chat_id)
+                                except:
+                                    return
+                                break
                     else:
                         return await message.reply_text(_["admin_11"].format(count))
                 else:
@@ -90,21 +66,19 @@ async def skip(cli, message: Message, _, chat_id):
         try:
             popped = check.pop(0)
             if popped:
-                await auto_clean(popped)
+                rem = popped["file"]
+                autoclean.remove(rem)
             if not check:
-                await auto_turn(chat_id, popped)
-                check = db.get(chat_id)
-                if not check:
-                    await message.reply_text(
-                        text=_["admin_6"].format(
-                            message.from_user.mention, message.chat.title
-                        ),
-                        reply_markup=close_markup(_),
-                    )
-                    try:
-                        return await Sagar.stop_stream(chat_id)
-                    except:
-                        return
+                await message.reply_text(
+                    text=_["admin_6"].format(
+                        message.from_user.mention, message.chat.title
+                    ),
+                    reply_markup=close_markup(_),
+                )
+                try:
+                    return await Anony.stop_stream(chat_id)
+                except:
+                    return
         except:
             try:
                 await message.reply_text(
@@ -113,12 +87,13 @@ async def skip(cli, message: Message, _, chat_id):
                     ),
                     reply_markup=close_markup(_),
                 )
-                return await Sagar.stop_stream(chat_id)
+                return await Anony.stop_stream(chat_id)
             except:
                 return
     queued = check[0]["file"]
     title = (check[0]["title"]).title()
     user = check[0]["by"]
+    user_id = check[0]["user_id"]
     streamtype = check[0]["streamtype"]
     videoid = check[0]["vidid"]
     status = True if str(streamtype) == "video" else None
@@ -138,14 +113,13 @@ async def skip(cli, message: Message, _, chat_id):
         except:
             image = None
         try:
-            await Sagar.skip_stream(chat_id, link, video=status, image=image)
+            await Anony.skip_stream(chat_id, link, video=status, image=image)
         except:
             return await message.reply_text(_["call_6"])
         button = stream_markup(_, chat_id)
-        img = await get_thumb(videoid, chat_id=chat_id)
+        img = await get_thumb(videoid,user_id)
         run = await message.reply_photo(
             photo=img,
-            has_spoiler=True,
             caption=_["stream_1"].format(
                 f"https://t.me/{app.username}?start=info_{videoid}",
                 title[:23],
@@ -172,14 +146,13 @@ async def skip(cli, message: Message, _, chat_id):
         except:
             image = None
         try:
-            await Sagar.skip_stream(chat_id, file_path, video=status, image=image)
+            await Anony.skip_stream(chat_id, file_path, video=status, image=image)
         except:
             return await mystic.edit_text(_["call_6"])
         button = stream_markup(_, chat_id)
-        img = await get_thumb(videoid, chat_id=chat_id)
+        img = await get_thumb(videoid,user_id)
         run = await message.reply_photo(
             photo=img,
-            has_spoiler=True,
             caption=_["stream_1"].format(
                 f"https://t.me/{app.username}?start=info_{videoid}",
                 title[:23],
@@ -193,7 +166,7 @@ async def skip(cli, message: Message, _, chat_id):
         await mystic.delete()
     elif "index_" in queued:
         try:
-            await Sagar.skip_stream(chat_id, videoid, video=status)
+            await Anony.skip_stream(chat_id, videoid, video=status)
         except:
             return await message.reply_text(_["call_6"])
         button = stream_markup(_, chat_id)
@@ -201,7 +174,6 @@ async def skip(cli, message: Message, _, chat_id):
             photo=config.STREAM_IMG_URL,
             caption=_["stream_2"].format(user),
             reply_markup=InlineKeyboardMarkup(button),
-            has_spoiler=True,
         )
         db[chat_id][0]["mystic"] = run
         db[chat_id][0]["markup"] = "tg"
@@ -216,7 +188,7 @@ async def skip(cli, message: Message, _, chat_id):
             except:
                 image = None
         try:
-            await Sagar.skip_stream(chat_id, queued, video=status, image=image)
+            await Anony.skip_stream(chat_id, queued, video=status, image=image)
         except:
             return await message.reply_text(_["call_6"])
         if videoid == "telegram":
@@ -247,10 +219,9 @@ async def skip(cli, message: Message, _, chat_id):
             db[chat_id][0]["markup"] = "tg"
         else:
             button = stream_markup(_, chat_id)
-            img = await get_thumb(videoid, chat_id=chat_id)
+            img = await get_thumb(videoid,user_id)
             run = await message.reply_photo(
                 photo=img,
-                has_spoiler=True,
                 caption=_["stream_1"].format(
                     f"https://t.me/{app.username}?start=info_{videoid}",
                     title[:23],
@@ -261,8 +232,3 @@ async def skip(cli, message: Message, _, chat_id):
             )
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "stream"
-
-
-
-
-
