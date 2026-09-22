53d8450ce638cb0c224a99077767c9a11f3fee74
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-09-22 23:24:43 +0530

    Refactored: RishuMusic to codex

--- codex/plugins/sudo/autoend.py
+++ codex/plugins/sudo/autoend.py
@@ -1,24 +1,3 @@
-# Copyright (c) 2025 @SUDEEPBOTS <HellfireDevs>
-# Location: delhi,noida
-#
-# All rights reserved.
-#
-# This code is the intellectual SUDEEPBOTS.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: sudeepgithub@gmail.com
-
 from pyrogram import filters
 from pyrogram.types import Message
 
@@ -43,3 +22,4 @@ async def auto_end_stream(_, message: Message):
         await message.reply_text("» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴅɪsᴀʙʟᴇᴅ.")
     else:
         await message.reply_text(usage)
+
