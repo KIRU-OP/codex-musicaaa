81aa36936de7b83789892638c6ae29026362b1bb
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-09-22 23:24:16 +0530

    Refactored: RishuMusic to codex

--- codex/plugins/misc/watcher.py
+++ codex/plugins/misc/watcher.py
@@ -1,29 +1,8 @@
-# Copyright (c) 2025 @SUDEEPBOTS <HellfireDevs>
-# Location: delhi,noida
-#
-# All rights reserved.
-#
-# This code is the intellectual SUDEEPBOTS.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: sudeepgithub@gmail.com
-
 from pyrogram import filters
 from pyrogram.types import Message
 
 from codex import app
-from codex.core.call import Sagar
+from codex.core.call import Anony
 
 welcome = 20
 close = 30
@@ -32,4 +11,4 @@ close = 30
 @app.on_message(filters.video_chat_started, group=welcome)
 @app.on_message(filters.video_chat_ended, group=close)
 async def welcome(_, message: Message):
-    await Sagar.stop_stream_force(message.chat.id)
+    await Anony.stop_stream_force(message.chat.id)
