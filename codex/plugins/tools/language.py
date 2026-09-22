7368ec308bc6ea53b99ef8ea6b202e4ddce6a0c9
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-09-22 23:24:38 +0530

    Refactored: RishuMusic to codex

--- codex/plugins/tools/language.py
+++ codex/plugins/tools/language.py
@@ -1,26 +1,6 @@
-# Copyright (c) 2025 @SUDEEPBOTS <HellfireDevs>
-# Location: delhi,noida
-#
-# All rights reserved.
-#
-# This code is the intellectual SUDEEPBOTS.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: sudeepgithub@gmail.com
-
+from pykeyboard import InlineKeyboard
 from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram.types import InlineKeyboardButton, Message
 
 from codex import app
 from codex.utils.database import get_lang, set_lang
@@ -30,30 +10,26 @@ from strings import get_string, languages_present
 
 
 def lanuages_keyboard(_):
-    keyboard = []
-    row = []
-    for i in languages_present:
-        row.append(
-            InlineKeyboardButton(
-                text=languages_present[i],
-                callback_data=f"languages:{i}",
+    keyboard = InlineKeyboard(row_width=2)
+    keyboard.add(
+        *[
+            (
+                InlineKeyboardButton(
+                    text=languages_present[i],
+                    callback_data=f"languages:{i}",
+                )
             )
-        )
-        if len(row) == 2:
-            keyboard.append(row)
-            row = []
-    if row:
-        keyboard.append(row)
-    keyboard.append(
-        [
-            InlineKeyboardButton(
-                text=_["BACK_BUTTON"],
-                callback_data=f"settingsback_helper",
-            ),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close"),
+            for i in languages_present
         ]
     )
-    return InlineKeyboardMarkup(keyboard)
+    keyboard.row(
+        InlineKeyboardButton(
+            text=_["BACK_BUTTON"],
+            callback_data=f"settingsback_helper",
+        ),
+        InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close"),
+    )
+    return keyboard
 
 
 @app.on_message(filters.command(["lang", "setlang", "language"]) & ~BANNED_USERS)
