d48b98344453a2f2bfc83a1ef618640846784bea
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-09-22 23:24:40 +0530

    Refactored: RishuMusic to codex

--- codex/plugins/sudo/logger.py
+++ codex/plugins/sudo/logger.py
@@ -1,25 +1,3 @@
-# Copyright (c) 2025 @SUDEEPBOTS <HellfireDevs>
-# Location: delhi,noida
-#
-# All rights reserved.
-#
-# This code is the intellectual SUDEEPBOTS.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: sudeepgithub@gmail.com
-
-
 from pyrogram import filters
 
 from codex import app
@@ -44,8 +22,4 @@ async def logger(client, message, _):
     else:
         await message.reply_text(usage)
 
-@app.on_message(filters.command(["cookies"]) & SUDOERS)
-@language
-async def logger(client, message, _):
-    await message.reply_document("cookies/logs.csv")
-    await message.reply_text("Please check given file to cookies file choosing logs...")
+
