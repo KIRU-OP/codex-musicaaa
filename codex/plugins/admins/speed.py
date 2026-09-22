05e927265ab6db15f52d049d87329d16676c402b
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-09-22 23:23:57 +0530

    Refactored: RishuMusic to codex

--- codex/plugins/admins/speed.py
+++ codex/plugins/admins/speed.py
@@ -1,29 +1,8 @@
-# Copyright (c) 2025 @SUDEEPBOTS <HellfireDevs>
-# Location: delhi,noida
-#
-# All rights reserved.
-#
-# This code is the intellectual SUDEEPBOTS.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: sudeepgithub@gmail.com
-
 from pyrogram import filters
 from pyrogram.types import Message
 
 from codex import app
-from codex.core.call import Sagar
+from codex.core.call import Anony
 from codex.misc import SUDOERS, db
 from codex.utils import AdminRightsCheck
 from codex.utils.database import is_active_chat, is_nonadmin_chat
@@ -115,7 +94,7 @@ async def del_back_playlist(client, CallbackQuery, _):
         text=_["admin_32"].format(CallbackQuery.from_user.mention),
     )
     try:
-        await Sagar.speedup_stream(
+        await Anony.speedup_stream(
             chat_id,
             file_path,
             speed,
