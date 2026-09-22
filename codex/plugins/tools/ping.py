c2c1bc43177fc32d1781ecec6d950b46a7fe8a52
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-09-22 23:24:26 +0530

    Refactored: RishuMusic to codex

--- codex/plugins/tools/ping.py
+++ codex/plugins/tools/ping.py
@@ -1,31 +1,10 @@
-# Copyright (c) 2025 @SUDEEPBOTS <HellfireDevs>
-# Location: delhi,noida
-#
-# All rights reserved.
-#
-# This code is the intellectual SUDEEPBOTS.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: sudeepgithub@gmail.com
-
 from datetime import datetime
 
 from pyrogram import filters
 from pyrogram.types import Message
 
 from codex import app
-from codex.core.call import Sagar
+from codex.core.call import Anony
 from codex.utils import bot_sys_stats
 from codex.utils.decorators.language import language
 from codex.utils.inline import supp_markup
@@ -40,7 +19,7 @@ async def ping_com(client, message: Message, _):
         photo=PING_IMG_URL,
         caption=_["ping_1"].format(app.mention),
     )
-    pytgping = await Sagar.ping()
+    pytgping = await Anony.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
