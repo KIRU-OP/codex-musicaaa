9ec2264c5e7650655c72eff073d5bcc0b49d2bb1
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-09-22 23:23:16 +0530

    Refactored: RishuMusic to codex

--- codex/utils/decorators/language.py
+++ codex/utils/decorators/language.py
@@ -1,31 +1,8 @@
-# Copyright (c) 2025 @SUDEEPBOTS <HellfireDevs>
-# Location: delhi,noida
-#
-# All rights reserved.
-#
-# This code is the intellectual SUDEEPBOTS.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: sudeepgithub@gmail.com
-
-from strings import get_string
-
 from codex import app
 from codex.misc import SUDOERS
-from config import SUPPORT_CHAT
 from codex.utils.database import get_lang, is_maintenance
-
+from strings import get_string
+from config import SUPPORT_CHAT
 
 def language(mystic):
     async def wrapper(_, message, **kwargs):
