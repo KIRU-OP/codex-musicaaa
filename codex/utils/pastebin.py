78308153ae94f643d95a0b6cb08008269f4619a9
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-09-22 23:22:59 +0530

    Refactored: RishuMusic to codex

--- codex/utils/pastebin.py
+++ codex/utils/pastebin.py
@@ -1,24 +1,3 @@
-# Copyright (c) 2025 @SUDEEPBOTS <HellfireDevs>
-# Location: delhi,noida
-#
-# All rights reserved.
-#
-# This code is the intellectual SUDEEPBOTS.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: sudeepgithub@gmail.com
-
 import aiohttp
 
 BASE = "https://batbin.me/"
@@ -34,7 +13,7 @@ async def post(url: str, *args, **kwargs):
         return data
 
 
-async def SagarBin(text):
+async def AnonyBin(text):
     resp = await post(f"{BASE}api/v2/paste", data=text)
     if not resp["success"]:
         return
